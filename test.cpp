c43cff86b79c7f96375e582b4fb66a2d794365da
dotdotot <dotdotot203@naver.com>
2022-10-04 19:16:44 +0900

    student test

--- test.cpp
+++ test.cpp
@@ -1,22 +1,28 @@
-#include <iostream>
-#include <string>
-using namespace std;
-int main()
+#include <stdio.h>
+#define SIZE 10
+int main(void)
 {
-    string password;
-    cin >> password;
-    string pa;
-    cin >> pa;
-    string pppp = pa;
-
-    if (password == pa)
+    int list[SIZE] = {3, 2, 9, 7, 1, 4, 8, 0, 6, 5};
+    int i, j, temp, least;
+    for (i = 0; i < SIZE - 1; i++)
     {
-        cout << "동일합니다\n";
-    }
-    else
-    {
-        cout << "동일x\n";
-    }
+        least = i;
+        for (j = i + 1; j < SIZE; j++)
+            if (list[j] < list[least])
+                least = j;
+        temp = list[i];
+        list[i] = list[least];
+        list[least] = temp;
 
-    cout <<pppp;
+        printf("%d pass \n", i + 1);
+        for (int test = 0; test < SIZE; test++)
+        {
+            printf("%d ", list[test]);
+        }
+        printf("\n");
+    }
+    for (i = 0; i < SIZE; i++)
+        printf("%d ", list[i]);
+    printf("\n");
+    return 0;
 }
